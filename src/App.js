0e1926f824d6784eba6e2786856fa0434a9b8dec
Ethan Porter <portere017@gmail.com>
2023-01-07 14:36:38 -0500

    added all components and updated app.js to call those components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Add code to import the components
+
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
+//Here, you are importing different components,adding a bootstrap container that helps you center your App on the page
+//Adding a title
+//Adding a Bootstrap row
+//Adding a column within the row for each of your components so far
+//Imported and Rendered the AllocationForm
+//Imported AppProvider and Nested components in the AppProvider element.
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
+                </div>
             </div>
         </AppProvider>
     );
 };
+
 export default App;
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,14 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+//Here, you are using the Bootstrap Alert classes to give a nice gray background by adding some text and hard coding a value.
+const Budget = () => {
+    const { budget } = useContext(AppContext);
 
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
